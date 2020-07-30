612eafde90999b33c3d0421fdd5581547e45293a
hiagopsilva <hiagohps360@gmail.com>
2020-07-30 04:18:39 -0300

    add navigate routes

---
+++ src/pages/Repository/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useRouteMatch } from 'react-router-dom';
+
+// import { Container } from './styles';
+
+interface RepositoryParams {
+  repository: string;
+}
+const Repository: React.FC = () => {
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  return (
+    <div>
+      <h1>
+        Repository:
+        {params.repository}
+      </h1>
+    </div>
+  );
+};
+
+export default Repository;
